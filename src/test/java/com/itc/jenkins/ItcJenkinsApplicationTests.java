86c5f38eb441747787b4ab09bd6cc6a2e4ef1dc4
Aman.Inamdar <aman.inamdar@itcinfotech.com>
2024-07-25 06:07:31 +0000

    first commit

---
+++ src/test/java/com/itc/jenkins/ItcJenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.itc.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+class ItcJenkinsApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(ItcJenkinsApplication.class);
+
+	
+	//lifecycle hooks
+	
+	
+
+	@Test
+	void contextLoads() {
+		logger.info("test case executed");
+		assertEquals(true,true);
+	}
+
+}
